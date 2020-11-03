aa81060ee68cd3a52267613f2266eba2d29c3b59
Parkduksung <duksung1234@naver.com>
2020-11-03 18:59:18 +0900

    blur 처리를 좀더 진하게 하는 공식 적용.

--- app/src/main/java/com/example/tensoroid/util/ImageUtils.kt
+++ app/src/main/java/com/example/tensoroid/util/ImageUtils.kt
@@ -80,6 +80,8 @@ object ImageUtils {
 
         renderScript.destroy()
 
+        blurFast(result1,blurRadius.toInt())
+
 
         val result = Bitmap.createBitmap(original.width, original.height, Bitmap.Config.ARGB_8888)
         val mCanvas = Canvas(result)
@@ -130,5 +132,33 @@ object ImageUtils {
         matrix.postRotate(90f)
 
         return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
+    }
+
+    private fun blurFast(bmp: Bitmap, radius: Int) {
+        val w = bmp.width
+        val h = bmp.height
+        val pix = IntArray(w * h)
+        bmp.getPixels(pix, 0, w, 0, 0, w, h)
+        var r = radius
+        while (r >= 1) {
+            for (i in r until h - r) {
+                for (j in r until w - r) {
+                    val tl = pix[(i - r) * w + j - r]
+                    val tr = pix[(i - r) * w + j + r]
+                    val tc = pix[(i - r) * w + j]
+                    val bl = pix[(i + r) * w + j - r]
+                    val br = pix[(i + r) * w + j + r]
+                    val bc = pix[(i + r) * w + j]
+                    val cl = pix[i * w + j - r]
+                    val cr = pix[i * w + j + r]
+                    pix[i * w + j] = -0x1000000 or (
+                            (tl and 0xFF) + (tr and 0xFF) + (tc and 0xFF) + (bl and 0xFF) + (br and 0xFF) + (bc and 0xFF) + (cl and 0xFF) + (cr and 0xFF) shr 3 and 0xFF) or (
+                            (tl and 0xFF00) + (tr and 0xFF00) + (tc and 0xFF00) + (bl and 0xFF00) + (br and 0xFF00) + (bc and 0xFF00) + (cl and 0xFF00) + (cr and 0xFF00) shr 3 and 0xFF00) or (
+                            (tl and 0xFF0000) + (tr and 0xFF0000) + (tc and 0xFF0000) + (bl and 0xFF0000) + (br and 0xFF0000) + (bc and 0xFF0000) + (cl and 0xFF0000) + (cr and 0xFF0000) shr 3 and 0xFF0000)
+                }
+            }
+            r /= 2
+        }
+        bmp.setPixels(pix, 0, w, 0, 0, w, h)
     }
 }
